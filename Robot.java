0e530fdca48215a3939e9e9a897a07033a9b7978
JP Beej <eggnogwhale@gmail.com>
2020-03-12 12:31:44 -0400

    Removed useless variable

--- Robot.java
+++ Robot.java
@@ -11,38 +11,30 @@ import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
-
 public class Robot extends TimedRobot {
- 
-  private int x = 10;
 
-  @Override
-  public void robotInit() {
-    
-  }
+    @Override
+    public void robotInit() {
 
- 
-   
-  @Override
-  public void robotPeriodic() {
-  }
+    }
 
- 
-  @Override
-  public void autonomousInit() {
-  }
+    @Override
+    public void robotPeriodic() {
+    }
 
-  
-   
-  @Override
-  public void autonomousPeriodic() {
-  }
+    @Override
+    public void autonomousInit() {
+    }
 
-  @Override
-  public void teleopPeriodic() {
-  }
+    @Override
+    public void autonomousPeriodic() {
+    }
 
-  @Override
-  public void testPeriodic() {
-  }
+    @Override
+    public void teleopPeriodic() {
+    }
+
+    @Override
+    public void testPeriodic() {
+    }
 }
